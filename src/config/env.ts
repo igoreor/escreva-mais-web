1a0ba888712d310df3befbe5dbac1569af98eabd
igoreor <igoreor24@gmail.com>
2025-10-03 16:21:04 -0300

    refactor(Environment): remover método privado de obtenção de variáveis de ambiente

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,10 +1,6 @@
 class Environment {
-  private get(key: string, defaultValue: string = ''): string {
-    return process.env[key] || defaultValue;
-  }
-
   get apiUrl(): string {
-    const url = this.get('NEXT_PUBLIC_API_BASE_URL');
+    const url = process.env.NEXT_PUBLIC_API_BASE_URL;
 
     if (!url && process.env.NODE_ENV === 'production') {
       console.warn('API URL não configurada em produção!');
